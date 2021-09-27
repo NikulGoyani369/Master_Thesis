6484652cd5f7340f53a0c5b44bd34e512622942c
Nikul Goyani <goyaniniks@gmail.com>
2021-09-27 23:49:10 +0200

    change description height

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ resultAndExplanationHTML = """
 """
 
 st.image(use_column_width=True, image=LOGO_URL)
-components.html(description, height=230, width=700, scrolling=True)
+components.html(description, height=240, width=700, scrolling=True)
 
 st.markdown("___")
 with st.form("my_form"):
