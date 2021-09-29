9b1fd6626b04c90e3b22cba509fa805539405e5f
Nikul Goyani <goyaniniks@gmail.com>
2021-09-29 17:05:56 +0200

    change the dataset pathr

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,9 +23,9 @@ def increment_counter():
 
 
 # this parth for Live searver
-FILEs = glob.glob("/Master_Thesis/semeval2013-Task7-2and3way/*/2way/*/*.xml") + \
+FILEs = glob.glob("/semeval2013-Task7-2and3way/*/2way/*/*.xml") + \
     glob.glob(
-        "/Master_Thesis/semeval2013-Task7-2and3way/test/2way/*/*/*.xml")
+        "/semeval2013-Task7-2and3way/test/2way/*/*/*.xml")
 
 try:
     f = open(f'{FILEs[st.session_state.count]}', 'r')
