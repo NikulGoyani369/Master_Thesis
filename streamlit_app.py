244eaba4d1b1a98876ff20acfc0f1615282f6fc9
Nikul Goyani <goyaniniks@gmail.com>
2021-10-04 20:45:14 +0200

    update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -135,11 +135,11 @@ with st.form("my_form"):
         # get string from next character
         res = str(i)[idx1 + len(sub1): idx2]
         realans.append(res)
-     if st.session_state.answer in realans:
+    if st.session_state.answer in realans:
         answerStat = "correct"
-     elif st.session_state.answer not in realans:
+    elif st.session_state.answer not in realans:
         answerStat = "incorrect"
-     response = openai.Completion.create(
+    response = openai.Completion.create(
         engine="davinci",
         prompt=f"Question: {Ques}\nStudentAnswer: {st.session_state.answer}\nTargetAnswer: {realans[0]}\nCorrect or Incorrect Explanation:",
         temperature=1,
@@ -148,7 +148,7 @@ with st.form("my_form"):
         frequency_penalty=2,
         presence_penalty=2,
         stop=["\n\n"]
-     )
+    )
 
 
 
