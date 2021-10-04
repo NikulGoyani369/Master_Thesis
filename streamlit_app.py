3c0e982c437a4bdc1495461c1dd32f2e64bb1082
Nikul Goyani <goyaniniks@gmail.com>
2021-10-04 20:22:55 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,8 @@ import os
 import pandas as pd
 import openai
 openai.organization = 'org-5p4uM0nHTES2niAIq4uMldR6'
-openai.api_key = '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'
+openai.api_key = os.getenv('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')
+# openai.api_key = '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'
 
 
 if 'count' not in st.session_state:
