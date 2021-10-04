b75a27052568d2fc4b56c8b6414423d4db8fe1f0
Nikul Goyani <goyaniniks@gmail.com>
2021-10-04 20:53:00 +0200

    update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,9 @@ import os
 import pandas as pd
 import openai
 openai.organization = 'org-5p4uM0nHTES2niAIq4uMldR6'
-openai.api_key = os.getenv('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')
+# openai.api_key = os.getenv('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')
 # openai.api_key = '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'
+openai.api_key = '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'
 
 
 if 'count' not in st.session_state:
@@ -135,11 +136,11 @@ with st.form("my_form"):
         # get string from next character
         res = str(i)[idx1 + len(sub1): idx2]
         realans.append(res)
-    if st.session_state.answer in realans:
-        answerStat = "correct"
-    elif st.session_state.answer not in realans:
-        answerStat = "incorrect"
-    response = openai.Completion.create(
+     if st.session_state.answer in realans:
+         answerStat = "correct"
+     elif st.session_state.answer not in realans:
+         answerStat = "incorrect"
+     response = openai.Completion.create(
         engine="davinci",
         prompt=f"Question: {Ques}\nStudentAnswer: {st.session_state.answer}\nTargetAnswer: {realans[0]}\nCorrect or Incorrect Explanation:",
         temperature=1,
@@ -148,7 +149,7 @@ with st.form("my_form"):
         frequency_penalty=2,
         presence_penalty=2,
         stop=["\n\n"]
-    )
+     )
 
 
 
