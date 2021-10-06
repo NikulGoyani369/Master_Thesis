3d980031fa3120f6a88ce35af0e433fb00e8f4e1
Nikul Goyani <goyaniniks@gmail.com>
2021-10-06 12:39:57 +0200

    update openAI API

--- streamlit_appcopy.py
+++ streamlit_appcopy.py
@@ -8,7 +8,6 @@ import openai
 # openai.organization = 'org-5p4uM0nHTES2niAIq4uMldR6'
 # openai.api_key = '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'
 openai.api_key = os.getenv("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")
-openai.Engine.retrieve("davinci")
 
 if 'count' not in st.session_state:
     st.session_state.count = 0
