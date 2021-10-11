bae5d3bb36c274b13af958cc37e022d5c1ff4bf9
Nikul Goyani <goyaniniks@gmail.com>
2021-10-11 16:13:03 +0200

    update steamlitfile

--- streamlit_appcopy.py
+++ streamlit_appcopy.py
@@ -177,7 +177,6 @@ def load_feedback_form():
         df2 = {'Question': Ques, 'student_answer': st.session_state.answer, 'correct_incorrect': answerStat,
                'explanation': explanation, 'rating': st.session_state.st, 'student_explanation': st.session_state.student_explanat}
         df.append(df2, ignore_index=True).to_csv("./data/j.csv", index=False)
-        st.write(df2)
 if isSubmitted:
     load_feedback_form()
 
