e6c43b9bb282391d8bedbf314e4f9a0d8fd5aaac
Nikul Goyani <goyaniniks@gmail.com>
2021-10-11 14:28:09 +0200

    update streamlitappcopy

--- streamlit_appcopy.py
+++ streamlit_appcopy.py
@@ -142,7 +142,7 @@ with st.form("my_form"):
         engine="davinci",
         prompt=f"Question: {Ques}\nStudentAnswer: {st.session_state.answer}\nTargetAnswer: {realans[0]}\n",
         temperature=1,
-        max_tokens=70,
+        max_tokens=64,
         top_p=1,
         frequency_penalty=2,
         presence_penalty=2,
@@ -179,7 +179,7 @@ def load_feedback_form():
         df2 = {'Question': Ques, 'student_answer': st.session_state.answer, 'correct_incorrect': answerStat,
                'explanation': explanation, 'rating': st.session_state.st, 'student_explanation': st.session_state.student_explanat}
         df.append(df2, ignore_index=True).to_csv("data/j.csv", index=False)
-        st.write(df)
+        st.write(df2)
 if isSubmitted:
     load_feedback_form()
 
