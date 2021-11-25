8c692b293c602a677261637c39ddac1ad37086fb
Nikul Goyani <goyaniniks@gmail.com>
2021-11-25 11:24:43 +0100

    update for feedback

--- tmp/streamlit_appcopy.py
+++ tmp/streamlit_appcopy.py
@@ -14,12 +14,15 @@ EXPLANATION_HTML = """
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@1,495&display=swap" rel="stylesheet">
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@300;400&display=swap" rel="stylesheet">
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     
 <div class="card mb-12 shadow-sm border-primary">
       <div class="card-body"  style='font-size:20px; text-align: justify; class=text-muted;'>
-        <h2 class="card-title">Result :</h2>
+        <h2 class="card-title"> Result :</h2>
         Your answer is {answerStatus}.
     </div>
 </div>
@@ -33,38 +36,50 @@ EXPLANATION_HTML = """
 <br>
 <div class="card mb-12 shadow-sm  border-primary">
       <div class="card-body" style='font-size:20px;text-align: justify; class=text-muted;'>
-        <h2 class="card-title">Explanation :</h2>
+        <h2 class="card-title"> Generated Explanation :</h2>
         {explanation}
     </div>
 </div>
 """
 EVOLUTION_HTML = """
+<div class="card border-primary">
 <div class="card mb-12 shadow-sm border-primary">
     <div class="card-header">
-        <h3 style="float:center; font-size:20px;text-align: justify; class=text-muted;">What do you think this explanation is good. Why not?</h3>
-        <br>
-        <h4 style="float:center; font-size:20px;text-align: justify; class=text-muted;">Below rating the explanation and write your Explanation</h4>
+    <div class="card-body" style='font-size:20px;text-align: justify; class=text-muted;font-family: 'IBM Plex Sans', sans-serif;'>
+         <p style="font-family: 'IBM Plex Sans', sans-serif; float:center; font-size:20px;text-align: justify; class=text-muted;"> For the generated explanation, you could complete the Evaluation based on rating and add your feedback in the section below.</p>
+    </div>
+        <ul style=" font-family: 'IBM Plex Sans', sans-serif;float:center;">
+            <li><p style="float:center; font-size:20px;text-align: justify; class=text-muted;">What do you think this explanation is good. Why not?</p></li>
+            <li><p style="float:center; font-size:20px;text-align: justify; class=text-muted;">What is wrong with this generated explanation?</p></li>
+            <li><p style="float:center; font-size:20px;text-align: justify; class=text-muted;">Does the generated explanation convey meaning of the original text?</p></li>
+            <li><p style="float:center; font-size:20px;text-align: justify; class=text-muted;">How useful did you find the generated explanation?</p></li>
+            <li><p style="float:center; font-size:20px;text-align: justify; class=text-muted;">Is generated explanation easy to understand?</p></li>
+            <li><p style="float:center; font-size:20px;text-align: justify; class=text-muted;">How fluent is generated explanation of the context? Does it read well?</p></li>
+        </ul>
     </div>
 </div>
+</div>
 """
+
+
 st.markdown("University Department:- [Language Technology Lab](https://www.ltl.uni-due.de)")
 
 LOGO_URL = "https://www.ltl.uni-due.de/assets/images/logo3.png"
 
 description = """
-<h2>Master Thesis Topic:- Collecting and analyse automatically generated feedback explanations</h2>
-<h3 style='font-weight: normal; font-family:'Open Sans', sans-serif;'>Contact Email-ID:- nikulkumar.goyani@stud.uni-due.de</h3>
+<h2 style='font-family: 'IBM Plex Sans', sans-serif;'>Master Thesis Topic:- Collecting and analyse automatically generated feedback explanations.</h2>
+<h3 style='font-weight: normal; font-family: 'IBM Plex Sans', sans-serif;'>Contact Email-ID:- nikulkumar.goyani@stud.uni-due.de</h3>
 <p style='outline-style: solid;padding:10px;outline-color: rgba(170, 50, 220, .6);font-size:20px; text-align: center; class=text-muted;font-family:'Open Sans', sans-serif; '> <b>PROTECTION OF DATA:-</b><br>
 <ul style='font-size:20px;class=text-muted;text-align: justify;'>
   <li> The dataset comprises student replies to explanation and definition questions found in practise activities and exams. 
   The Student Response Analysis dataset is split into two parts: Beetle and SciEntsBank. 
-  Answers to explanation and definition questions are meticulously labelled. The data set includes a sample question, reference answer, and student response.
+  Answers to explanation and definition questions are meticulously labelled. The data set includes an question, reference answer, and student response.
   In this thesis, a human annotator classifies each student response as one of five evaluations. 
   We used the SRA dataset to build our dataset. </li> <br>
   <li> Original principle of this thesis is to take an answers from the student, which can then be classified as either correct or incorrect.
   Based on the student answers, generate an explanation of why the student's answer is incorrect or correct. Firstly, it is a machine-generated explanation by
-  the model of Natural Language Processing (NLP). Finally, it asks the student to evaluate if the machine-generated explanation is viable or write a student explanation</li> <br>
-  <li> Whatever data is provided in this survey will be taken into use only for this thesis work. The data will not be shared in any place. This data will remain completly anonymous</li>
+  the model of Natural Language Processing (NLP). Finally, it asks the student to evaluate if the machine-generated explanation is viable or write a student feedback. </li> <br>
+  <li> Whatever data is provided in this survey will be taken into use only for this thesis work. The data will not be shared in any place. This data will remain completly anonymous.</li>
 </ul>
 </p>
 """
@@ -114,13 +129,13 @@ def clear_session_state():
 
 def create_question_string():
     # st.write("Question :")
-    return f"<h3 style=font-size:22px; class=text-muted;text-align: justify;display: none;'>Question :</h3>"\
+    return f"<h3 style='font-family: 'IBM Plex Sans', sans-serif; font-size:22px; class=text-muted;text-align: justify;display: none;'>Question :</h3>"\
            f"<p style='outline-style: solid; margin:auto; padding:15px;outline-color: rgba(170, 50, 220, .6);text-align: justify;font-size:25px;height: 50px; class=text-muted'>" \
            f"{data['question']}</p>"
 
 
 def create_answer_string():
-    return f"<h3 style=font-size:22px; class=text-muted;text-align: justify;display: none;'>Student Answer :</h3>"\
+    return f"<h3 style=' font-family: 'IBM Plex Sans', sans-serif; font-size:22px; class=text-muted;text-align: justify;display: none;'>Student Answer :</h3>"\
            f"<p style='outline-style: solid;padding:10px; outline-color: rgba(170, 50, 220, .6);font-size:25px; class=text-muted;height: 55px;text-align: justify;'>" \
            f" {data['student_answer']}</p>"
 
@@ -136,7 +151,7 @@ def load_student_question_form():
             explanation=data['explanation'],
             reference_ans=data['ref_answer']
         ),
-        height=500,
+        height=600,
         scrolling=True
     )
     st.write(
@@ -146,11 +161,10 @@ def load_student_question_form():
     radioOptions = ['1 Star', '2 Star', '3 Star', '4 Star', '5 Star']
 
     st.container()
-    # components.html(EVOLUTION_HTML.format(width=500))
     with st.form("form", clear_on_submit=True):
-        components.html(EVOLUTION_HTML.format())
-        st.radio("Select Rating", radioOptions, key=STUDENT_RATING)
-        st.text_area("Student Explanation", key=STUDENT_EXPLANATION)
+        components.html(EVOLUTION_HTML.format(), height=400,scrolling=True)
+        st.radio("Select Rating :", radioOptions, key=STUDENT_RATING)
+        st.text_area("Student Feedback :", key=STUDENT_EXPLANATION)
         st.form_submit_button(
             "Next Question", on_click=feedback_form_submitted)
 
@@ -192,7 +206,7 @@ data = {
 # first uni logo with description
 if enableLogo:
     st.image(use_column_width=True, image=LOGO_URL)
-    components.html(description, height=340, width=700, scrolling=True)
+    components.html(description, height=350, width=700, scrolling=True)
 
 st.markdown("___")
 question_str = create_question_string()
